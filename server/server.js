302aee3ce6346554d1874ff9d50b24b4ed67d235
Senuch-Uzair-Tariq <uzair.episodicx@gmail.com>
2018-09-16 14:12:24 +0500

    Messaging System

--- server/server.js
+++ server/server.js
@@ -1,17 +1,46 @@
 // Fetching Dependencies/Third parties
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
 // Declaring Server Variables
-const app = new express()
+const app = express()
 
-// Adding port flag
+// Adding Port Flag
 const port = process.env.PORT||3000
 
 // Settingup static content directory
 app.use(express.static(path.join(__dirname, '../public')))
 
+// Declaring HTTP server
+var server = http.createServer(app)
+
+// Adding SocketIO support
+var io = socketIO(server)
+
+// Events
+io.on('connection',(socket) => {
+  console.log('New user connected!')
+
+  socket.on('createMessage',function(message){
+    console.log('Create Message',message);
+
+    io.emit('newMessage',{
+      from:message.from,
+      message:message.message,
+      createdAt:new Date().getTime()
+    })
+
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  })
+})
+
+
 // Poweringup Server
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('App up and running!');
 })
